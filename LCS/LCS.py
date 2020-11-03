d87fd13e1bc7aab6e66d1d8fbfb7ed31d2d48eaa
Kotoricyann <lfr@ruc.edu.cn>
2020-11-03 16:49:46 +0800

    finis LSC

--- LCS/LCS.py
+++ LCS/LCS.py
@@ -1,6 +1,22 @@
 import math
 import numpy
 
+def printlist( str_one, str_two, record ):
+    len_str1 = len(str_one)
+    len_str2 = len(str_two)
+    print(' ',end=' ')
+    for i in range(len_str2):
+        print(str_two[i],end=' ')
+    print('')
+    for i in range(1,len_str1+1):
+        if i!=0:
+            print(str_one[i-1],end=' ')
+        else:
+            print(' ',end=' ')
+        for j in range(1,len_str2+1):
+            print(record[i][j], end=' ')
+        print('')
+
 def lcs(str_one, str_two, case_sensitive=True):
     len_str1 = len(str_one)
     len_str2 = len(str_two)
@@ -15,37 +31,47 @@ def lcs(str_one, str_two, case_sensitive=True):
                 record[i + 1][j + 1] = record[i + 1][j]
             else:
                 record[i + 1][j + 1] = record[i][j + 1]
-    for i in range(0,len_str1+1):
-        for j in range(0,len_str2+1):
-            print(record[i][j], end=' ')
-        print('')
+    printlist( str_one, str_two, record )
     key = []
     i = len_str1
     j = len_str2
     while record[i][j]!=0:
         if record[i][j]==record[i][j-1] and record[i][j]==record[i-1][j]:
-            key.append(0)
+            key.append(False)
             i=i-1
         elif record[i][j]==record[i][j-1]+1 and record[i][j]==record[i-1][j]+1 :
-            key.append(record[i][j])
+            key.append(str_one[i-1])
             i=i-1
             j=j-1
         elif record[i][j]==record[i][j-1]+1 and record[i][j]==record[i-1][j] :
-            key.append(0)
+            key.append(False)
             i=i-1
         elif record[i][j]==record[i][j-1] and record[i][j]==record[i-1][j]+1 :
-            key.append(0)
+            key.append(False)
             j=j-1
         else:
             print("error!")
             exit()
+    if i!=0 or j!=0:
+        key.append(False)
     
-    print(key)
-        
+    #print(key)
+    flag = 0
+    for num in range(len(key)-1,-1,-1):
+        #print(key[num])
+        #print (num)
+        if key[num]!=False:
+            print(key[num],end=' ')
+            flag = 0
+        else:
+            if flag==0:
+                print("\\w+",end=' ')
+                flag = 1
+    print('')
 
     return record[-1][-1]
  
 s1 = input()
 s2 = input()
 x = lcs(s1, s2)
-print(x)
+print ("最大子序列长度：", x)
---
+++ LCS/README.md
@@ -0,0 +1,2 @@
+# A LSC Program
+The program need to input two centences. Then program will ouput the maximum positives match.
