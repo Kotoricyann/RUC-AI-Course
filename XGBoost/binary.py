624e6c169daf37731001d14b3290ee37f95c6ba7
Kotoricyann <lfr@ruc.edu.cn>
2020-11-17 01:58:23 +0800

    finished

--- XGBoost/binary.py
+++ XGBoost/binary.py
@@ -1,14 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
-#import XGBoost as xgb
+import xgboost as xgb
+from sklearn import metrics
 
 
-trainfile = np.loadtxt('./dataset/train.csv', skiprows=1 , delimiter=',' )
-testfile = np.loadtxt('./dataset/test.csv', skiprows=1 , delimiter=',' )
-validationfile = np.loadtxt('./dataset/validation.csv', skiprows=1 , delimiter=',' )
+trainfile = np.loadtxt('XGBoost/dataset/train.csv', skiprows=1 , delimiter=',' )
+testfile = np.loadtxt('XGBoost/dataset/test.csv', skiprows=1 , delimiter=',' )
+validationfile = np.loadtxt('XGBoost/dataset/validation.csv', skiprows=1 , delimiter=',' )
 
-data = trainfile[:,0:-1]
-label = trainfile[:,-1]
+traindata = trainfile[:,0:-1]
+trainlabel = trainfile[:,-1]
+
+testdata = trainfile[:,0:-1]
+testlabel = trainfile[:,-1]
+
+validdata = trainfile[:,0:-1]
+validlabel = trainfile[:,-1]
+
+dtrain=xgb.DMatrix(traindata,label=trainlabel)
+dtest=xgb.DMatrix(testdata)
+dvalid=xgb.DMatrix(validdata,label=validlabel)
 
 params={'booster':'gbtree',
     'objective': 'binary:logistic',
@@ -21,14 +32,21 @@ params={'booster':'gbtree',
     'eta': 0.025,
     'seed':0,
     'nthread':8,
-    'silent':1,
-    'nthread': 4}
+    'silent':1,}
 
-eval_set = [(data,label)]
-print(eval_set)
+eval_set = [(dtrain,'train'), (dvalid,'valid')]
+#print(eval_set)
 
-#xgboost.train(params,data,num_boost_round=10,evals=eval_set )
+bst = xgb.train(params, dtrain,num_boost_round=10,evals=eval_set )
 
+ypred=bst.predict(dtest)
+# 设置阈值, 输出一些评价指标，选择概率大于0.5的为1，其他为0类
+y_pred = (ypred >= 0.5)*1
 
-
+print ('AUC: %.4f' % metrics.roc_auc_score(testlabel,ypred))
+print ('ACC: %.4f' % metrics.accuracy_score(testlabel,y_pred))
+print ('Recall: %.4f' % metrics.recall_score(testlabel,y_pred))
+print ('F1-score: %.4f' %metrics.f1_score(testlabel,y_pred))
+print ('Precesion: %.4f' %metrics.precision_score(testlabel,y_pred))
+print(metrics.confusion_matrix(testlabel,y_pred))
 
